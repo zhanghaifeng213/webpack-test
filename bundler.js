ae07221a94340f17e7a1345f88acc11063825076
zhf <zhanghf_123@sina.com>
2022-05-04 18:16:05 +0800

    bundler 入口文件分析

--- bundler.js
+++ bundler.js
@@ -1,13 +1,34 @@
 const fs = require("fs")
+const path = require("path")
 const parser = require("@babel/parser")
+const traverse = require("@babel/traverse").default
+const babel = require("@babel/core")
 
 const  moduleAnylaser = (filename) => {
   const content = fs.readFileSync(filename, 'utf-8')
   const ast = parser.parse(content, {
     sourceType: 'module'
   })
-  console.log(ast.program.body);
-  console.log(content);
+  const dependencies = {}
+  traverse(ast, {
+    ImportDeclaration({node}) {
+      // console.log(node);
+      const dirname = path.dirname(filename)
+      const newFile = './'+path.join(dirname, node.source.value)
+      dependencies[node.source.value] = newFile
+    }
+  })
+  // console.log('dependencies', dependencies);
+  const {code} = babel.transformFromAst(ast,null,{
+    presets: ["@babel/preset-env"]
+  })
+  // console.log('code', code);
+  return {
+    filename,
+    dependencies,
+    code
+  }
 }
 
-moduleAnylaser('./src/index.js')
+const moduleInfo = moduleAnylaser('./src/index.js')
+console.log("moduleInfo", moduleInfo);
